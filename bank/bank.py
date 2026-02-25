d7857f3240474e75dcaea6a9c5ad45f7af5c28dd
zuka910-arch <zuka910@gmail.com>
2026-02-25 09:51:30 +0000

    bank.py

---
+++ bank/bank.py
@@ -0,0 +1,18 @@
+def main():
+    greeting = input("greeting:").strip().lower()
+
+
+
+
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+
+main()
+
+
