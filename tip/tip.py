4e3e935221c31d085430d14bad89b328f086947f
zuka910-arch <zuka910@gmail.com>
2026-03-02 16:09:03 +0000

    tip.py

---
+++ tip/tip.py
@@ -0,0 +1,18 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    d_without_dollar = d.replace("$", "")
+    return float(d_without_dollar)
+
+
+def percent_to_float(p):
+    p_without_percent = p.replace("%", "")
+    return float(p_without_percent) / 100
+
+
+main()
