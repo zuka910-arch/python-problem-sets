c8482eaf7c673483c84c7d6fd00536e12ff975ba
zuka910-arch <zuka910@gmail.com>
2026-04-14 11:45:28 +0000

    add outdate.py

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,7 +15,7 @@ months = [
 
 while True:
     try:
-        date = input("Date: ").strip()
+        date = input("Date: ").strip().title()
         if "/" in date:
             m,d,y = date.split("/")
             m = int(m)
@@ -24,7 +24,15 @@ while True:
             if 1<= m <= 12 and 1 <= d <= 31:
                 print(f"{y}-{m:02}-{d:02}")
                 break
-
+        elif "," in date:
+            clean_date = date.replace(",", "")
+            m,d,y = clean_date.split(" ")
+            if m in months and 1<= m <= 12 and 1 <= d <= 31:
+                m_num = months.index(m) + 1
+                d = int(d)
+                y = int(y)
+                print(f"{y}-{m_num:02}-{d:02}")
+                break
 
     except:
         pass
