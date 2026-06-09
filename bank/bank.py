3b4e9364311c4d975ace6c408d10120cc21a552b
zuka910-arch <zuka910@gmail.com>
2026-06-09 20:40:47 +0000

    fixed repo

--- bank/bank.py
+++ bank/bank.py
@@ -1,18 +1,33 @@
 def main():
-    greeting = input("greeting:").strip().lower()
+    greeting = input("greeting:")
+    result = value(greeting)
+    print(f"${result}")
 
 
 
+def value(greeting):
+
+    greeting = greeting.strip().lower()
 
     if greeting.startswith("hello"):
-        print("$0")
+        return 0
     elif greeting.startswith("h"):
-        print("$20")
+        return 20
     else:
-        print("$100")
+        return 100
 
 
 
-main()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
 
 
